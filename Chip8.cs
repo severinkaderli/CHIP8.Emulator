42c9042db10a30cb0e0c4b7241f75c29b6a965a7
Severin Kaderli <severin.kaderli@gmail.com>
2016-06-21 20:26:51 +0200

    Added the rest of the opcodes.
    Signed-off-by: Severin Kaderli <severin.kaderli@gmail.com>

--- Chip8.cs
+++ Chip8.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,42 +12,41 @@ namespace Chip8
         /// <summary>
         /// Start of the working RAM.
         /// </summary>
-        private const int PROGRAMM_COUNTER_START = 0x200;
+        private const int START_PROGRAM_MEMORY = 512;
 
-        /// <summary>
-        /// If the draw flag is set, we draw something on the screen.
-        /// </summary>
-        protected bool drawFlag;
+        public bool isInputExecuted = false;
+
+        public byte lastInput;
 
         /// <summary>
         /// The memory of the CHIP-8.
         /// </summary>
-        protected byte[] memory = new Byte[4096];
+        protected byte[] memory = new byte[4096];
 
         /// <summary>
         /// The current opcode.
         /// </summary>
-        protected int opcode;
+        protected ushort opcode;
 
         /// <summary>
         /// CPU registers from V0 to VE. VF is the carry flag.
         /// </summary>
-        protected int[] v = new int[16];
+        protected byte[] V = new byte[16];
 
         /// <summary>
         /// The index register
         /// </summary>
-        protected int i;
+        protected ushort I = 0;
 
         /// <summary>
         /// Program counter
         /// </summary>
-        protected int pc;
+        protected ushort pc;
 
         /// <summary>
         /// Array that contains the current screen state.
         /// </summary>
-        protected bool[] gfx = new bool[64 * 32];
+        public bool[,] gfx = new bool[64, 32];
 
         /// <summary>
         /// If greater than zero, this timer counts down.
@@ -61,7 +61,7 @@ namespace Chip8
         /// <summary>
         /// The stack
         /// </summary>
-        protected ushort[] stack = new ushort[16];
+        protected Stack<ushort> stack = new Stack<ushort>(16);
 
         /// <summary>
         /// Pointer that point to the current stack position.
@@ -71,8 +71,11 @@ namespace Chip8
         /// <summary>
         /// The current state of the keypad.
         /// </summary>
-        protected char[] keyState = new char[16];
+        public byte[] key = new byte[16];
 
+        /// <summary>
+        /// The fontset of the CHIP8.
+        /// </summary>
         byte[] fontset = new byte[80]
         {
             0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
@@ -97,79 +100,346 @@ namespace Chip8
         /// Runs the emulator.
         /// </summary>
         /// <param name="path">Path to the game</param>
-        public void Run(String path)
-        {
-            // Initialize the system
-            Initialize();
+        ////public void Run(String path)
+        ////{
+        ////    // Initialize the system
+        ////    //Initialize();
 
-            // Load the game
-            LoadGame(path);
+        ////    // Load the game
+        ////    LoadGame(path);
      
-            // Main execution loop
-            for (;;)
-            {
-                EmulateCycle();
+        ////    // Main execution loop
+        ////    for (;;)
+        ////    {
+        ////        EmulateCycle();
 
-                if(drawFlag)
-                {
-                    DrawGraphics();
-                }
+        ////        if(drawFlag)
+        ////        {
+        ////            DrawGraphics();
+        ////        }
 
-                SetKeys();
-            }
-        }
+        ////        SetKeys();
+        ////    }
+        ////}
 
         /// <summary>
         /// Initializes the system.
         /// </summary>
-        private void Initialize()
+        public void Initialize(String name)
         {
-            i = 0;
+            I = 0;
             stackPointer = 0;
 
-            pc = PROGRAMM_COUNTER_START;
+            pc = START_PROGRAM_MEMORY;
+
+            LoadGame(name);
 
             // Load fontset into memory
-            for(int i = 0; i< 80; ++i)
+            for(int i = 0; i < fontset.Length; ++i)
             {
                 memory[i] = fontset[i];
             }
         }
 
-        // Loads the game into memory
+        /// <summary>
+        /// Loads a game into memory
+        /// </summary>
+        /// <param name="game"></param>
         private void LoadGame(String game)
         {
+            // Open the file in binary mode
+            BinaryReader binaryReader = new BinaryReader(File.Open(game, FileMode.Open));
 
-            byte[] Buffer = System.IO.File.ReadAllBytes(game);
-
-            for(int i = 0; i < Buffer.Length; ++i)
+            // Loading the game into memory
+            for(int i = 0; i < binaryReader.BaseStream.Length; i++)
             {
-                memory[i + 512] = Buffer[i];
+                memory[START_PROGRAM_MEMORY + i] = binaryReader.ReadByte();
             }
         }
 
-        private void EmulateCycle()
+        public void EmulateCycle()
         {
             // Fetch the current opcode
-            opcode = memory[pc] << 8 | memory[pc + 1];
+            opcode = (ushort)((ushort)(memory[pc] << 8) | memory[pc + 1]);
+            Console.WriteLine("Executing opcode: {0:X}", opcode);
+
+            ushort X = (byte)(memory[pc] & 0x0F);
+            ushort Y = (byte)(memory[pc + 1] >> 4);
+            ushort N = (byte)(memory[pc + 1] & 0x0F);
+            ushort NN = (byte)(memory[pc + 1]);
+            ushort NNN = (byte)(opcode & 0x0FFF);
 
             // Execute the opcode
-            switch(opcode & 0xF000)
+            switch (opcode & 0xF000)
             {
-                case 0xA000: // ANNN: Sets I to the address NNN
+                case 0x0000:
+                    if (X == 0x0)
+                    {
+                        if (N == 0x0)
+                        {
+                            //Clear the display.
+                            gfx = new bool[64, 32];
+                        }
+                        if (N == 0xE)
+                        {
+                            //Return from subroutine.
+                            pc = stack.Pop();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("RCA 1802 CODE -- NOT IPLEMENTED");
+                    }
 
-                    i = opcode & 0x0FFF;
-                    pc += 2;
+                    break;
+
+                case 0x1000: // 1NNN: Jump to the address NNN
+                    pc = (ushort)((opcode & 0x0FFF) - 2);
+                    break;
+
+                case 0x2000: // 2NNN: Jump to the subroutine in address NNN
+                    stack.Push(pc);
+                    pc = (ushort)((opcode & 0x0FFF) - 2);
+                    Console.WriteLine("NNN: {0:X}", NNN);
+                    break;
+
+                case 0x3000: // 3XNN: Skip the next instruction if value of register X equals NN
+                    if(V[X] == NN)
+                    {
+                        pc += 2;
+                    }
+                    break;
+
+                case 0x4000: // 4XNN: Skip the next instruction if value of register X not equals NNN
+                    if(V[X] != memory[pc + 1])
+                    {
+                        pc += 2;
+                    }
+                    break;
+
+                case 0x5000: // 5XYN: Skip the next instruction if value of register X and Y are equal
+                    if(V[X] == V[Y])
+                    {
+                        pc += 2;
+                    }
+                    break;
+
+                case 0x6000: // 6XNN: Sets value of register X to NN
+                    V[X] = (byte)NN;
+                    break;
+
+                case 0x7000: // 7XNN: Increments value of register X by NN
+                    V[X] += (byte)NN;
+                    break;
+
+                case 0x8000:
+                    switch (N)
+                    {
+                        case 0x0: // 8XY0
+                            V[X] = V[Y];
+                            break;
+
+                        case 0x1: // 8XY1
+                            V[X] |= V[Y];
+                            break;
+
+                        case 0x2: // 8XY2
+                            V[X] &= V[Y];
+                            break;
+
+                        case 0x3: // 8XY3
+                            V[X] ^= V[Y];
+                            break;
+
+                        case 0x4: // 8XY4
+                            if ((V[X] + V[Y]) > 255)
+                            {
+                                V[0xF] = 1;
+                            } else
+                            {
+                                V[0xF] = 0;
+                            }
+
+                            V[X] = (byte)(V[X] + V[Y]);
+
+                            break;
+
+                        case 0x5: // 8XY5
+                            if(V[X] > V[Y])
+                            {
+                                V[0xF] = 1;
+                            } else {
+                                V[0xF] = 0;
+                            }
+
+                            V[X] = (byte)(V[X] - V[Y]);
+
+                            break;
+
+                        case 0x6: // 8XY6
+                            V[0xF] = (byte)(V[X] & 0x1);
+                            V[X] >>= 1;
+
+                            break;
+
+                        case 0x7: // 8XY7
+                            if(V[Y] > V[X])
+                            {
+                                V[0xF] = 1;
+                            } else
+                            {
+                                V[0xF] = 0;
+                            }
+
+                            V[X] = (byte)(V[Y] - V[X]);
+
+                            break;
+
+                        case 0xE: // 8XYE
+                            V[0xF] = (byte)(V[X] >> 7);
+                            V[X] <<= 1;
+
+                            break;
+                    }
+                    break;
+
+                case 0x9000: // 9NNN: Skip next instruction if register X and Y are not equal.
+                    if(V[X] != V[Y]) {
+                        pc += 2;
+                    }
+                    break;
+
+                case 0xA000: // ANNN: Sets I to the address NNN
+                    I = NNN;
+                    break;
+
+                case 0xB000:
+                    pc = (ushort)((opcode & 0x0FFF) + V[0] - 2);
+                    break;
+
+                case 0xC000:
+                    Random rnd = new Random();
+                    V[X] = (byte)(rnd.Next(255) & NN);
+                    break;
+
+                case 0xD000: // Print sprite to screen
+                    V[0xF] = 0;
+
+                    for(int height = 0; height < N; height++)
+                    {
+                        byte spritePart = memory[I + height];
+
+                        for(int width = 0; width < 8; width++)
+                        {
+                            if((spritePart & (0x80 >> width)) != 0)
+                            {
+                                ushort _x = (ushort)((V[X] + width) % 64);
+                                ushort _y = (ushort)((V[X] + width) % 32);
+
+                                if(gfx[_x, _y] == true)
+                                {
+                                    V[0xF] = 1;
+                                }
+
+                                gfx[_x, _y] ^= true;
+                            }
+                        }
+                    }
+                    break;
+
+                case 0xE000:
+
+                    if(N == 0xE)
+                    {
+                        if(key[V[X]] == 1)
+                        {
+                            pc += 2;
+                        }
+                    }
+                    else
+                    {
+                        if(key[V[X]] == 0)
+                        {
+                            pc += 2;
+                        }
+                    }
+
+                    break;
+
+                case 0xF000:
+                    switch(N)
+                    {
+                        case 0x7: // Set Vx to delay timer
+                            V[X] = delayTimer;
+                            break;
+
+                        case 0xA: // Do not increase program counter until a key is pressed. Then store pressed key.
+                            if (isInputExecuted)
+                            {
+                                V[X] = lastInput;
+                            }
+                            else
+                            {
+                                pc -= 2;
+                            }
+
+                            break;
+
+                        case 0x5:
+                            switch (Y)
+                            {
+                                case 0x1: // Delay Timer = regiserX
+                                    delayTimer = (byte) V[X];
+                                    break;
+
+                                case 0x5: // Store registers V0 through Vx in memory starting at location I.
+
+                                    for (int i = 0; i <= X; i++)
+                                    {
+                                        memory[I + i] = V[i];
+                                    }
+                                    break;
+
+                                case 0x6: // Read registers V0 through Vx from memory starting at location I.
+                                    for (int i = 0; i <= X; i++)
+                                    {
+                                        V[i] = memory[I + i];
+                                    }
+                                    break;
+                            }
+                            break;
+
+                        case 0x8: //Sound Timer = registerX
+                            soundTimer = V[X];
+                            break;
+
+                        case 0xE: //I = I + regiserX
+                            I += V[X];
+                            break;
+
+                        case 0x9: //Set I to the location of char in registerX
+                            I = (ushort)(V[X] * 5);
+                            break;
+
+                        case 0x3: // Store BCD representation of Vx in memory locations I, I+1, and I+2.
+                            decimal number = V[X];
+                            memory[I] = (byte)(number / 100);
+                            memory[I + 1] = (byte)((number % 100) / 10);
+                            memory[I + 2] = (byte)((number % 100) % 10);
+                            break;
+                    }
 
                     break;
 
                 default:
-                    Console.WriteLine("Unknown opcode: " + opcode);
-
+                    Console.WriteLine("Unknown opcode: {0:X}", opcode);
                     break;
             }
 
-            if(delayTimer > 0)
+            isInputExecuted = false;
+            pc += 2;
+
+            if (delayTimer > 0)
             {
                 delayTimer--;
             }
@@ -179,7 +449,7 @@ namespace Chip8
                 soundTimer--;
                 if(soundTimer == 0)
                 {
-                    // BEEP
+                    Console.Beep();
                 }
             }
         }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+using System.Drawing;
 
 namespace Chip8
 {
@@ -12,7 +13,216 @@ namespace Chip8
     {
         static void Main(string[] args)
         {
-            Chip8 chip8 = new Chip8();
+            using (var gameWindow = new GameWindow())
+            {
+                int frame = 0;
+
+                var chip = new Chip8();
+                chip.Initialize("breakout.ch8");
+
+                gameWindow.Load += (sender, e) =>
+                {
+                    gameWindow.VSync = VSyncMode.On;
+                };
+
+                gameWindow.Resize += (sender, e) =>
+                {
+                    GL.Viewport(0, 0, 640, 320);
+                };
+
+                gameWindow.KeyDown += (sender, e) =>
+                {
+                    var pressedKey = e.Key.ToString();
+
+                    switch (pressedKey)
+                    {
+                        case "1":
+                            chip.key[0x1] = 1;
+                            chip.lastInput = 0x1;
+                            break;
+                        case "2":
+                            chip.key[0x2] = 1;
+                            chip.lastInput = 0x2;
+                            break;
+                        case "3":
+                            chip.key[0x3] = 1;
+                            chip.lastInput = 0x3;
+                            break;
+                        case "4":
+                            chip.key[0xC] = 1;
+                            chip.lastInput = 0xC;
+                            break;
+                        case "q":
+                        case "Q":
+                            chip.key[0x4] = 1;
+                            chip.lastInput = 0x4;
+                            break;
+                        case "w":
+                        case "W":
+                            chip.key[0x5] = 1;
+                            chip.lastInput = 0x5;
+                            break;
+                        case "e":
+                        case "E":
+                            chip.key[0x6] = 1;
+                            chip.lastInput = 0x6;
+                            break;
+                        case "r":
+                        case "R":
+                            chip.key[0xD] = 1;
+                            chip.lastInput = 0xD;
+                            break;
+                        case "a":
+                        case "A":
+                            chip.key[0x7] = 1;
+                            chip.lastInput = 0x7;
+                            break;
+                        case "s":
+                        case "S":
+                            chip.key[0x8] = 1;
+                            chip.lastInput = 0x8;
+                            break;
+                        case "d":
+                        case "D":
+                            chip.key[0x9] = 1;
+                            chip.lastInput = 0x9;
+                            break;
+                        case "f":
+                        case "F":
+                            chip.key[0xE] = 1;
+                            chip.lastInput = 0xE;
+                            break;
+                        case "z":
+                        case "Z":
+                            chip.key[0xA] = 1;
+                            chip.lastInput = 0xA;
+                            break;
+                        case "x":
+                        case "X":
+                            chip.key[0x0] = 1;
+                            chip.lastInput = 0x0;
+                            break;
+                        case "c":
+                        case "C":
+                            chip.key[0xB] = 1;
+                            chip.lastInput = 0xB;
+                            break;
+                        case "v":
+                        case "V":
+                            chip.key[0xF] = 1;
+                            chip.lastInput = 0xF;
+                            break;
+                    }
+
+                    chip.isInputExecuted = true;
+                };
+
+                gameWindow.KeyUp += (sender, e) =>
+                {
+                    var pressedKey = e.Key.ToString();
+
+                    switch (pressedKey)
+                    {
+                        case "1":
+                            chip.key[0x1] = 0;
+                            break;
+                        case "2":
+                            chip.key[0x2] = 0;
+                            break;
+                        case "3":
+                            chip.key[0x3] = 0;
+                            break;
+                        case "4":
+                            chip.key[0xC] = 0;
+                            break;
+                        case "q":
+                        case "Q":
+                            chip.key[0x4] = 0;
+                            break;
+                        case "w":
+                        case "W":
+                            chip.key[0x5] = 0;
+                            break;
+                        case "e":
+                        case "E":
+                            chip.key[0x6] = 0;
+                            break;
+                        case "r":
+                        case "R":
+                            chip.key[0xD] = 0;
+                            break;
+                        case "a":
+                        case "A":
+                            chip.key[0x7] = 0;
+                            break;
+                        case "s":
+                        case "S":
+                            chip.key[0x8] = 0;
+                            break;
+                        case "d":
+                        case "D":
+                            chip.key[0x9] = 0;
+                            break;
+                        case "f":
+                        case "F":
+                            chip.key[0xE] = 0;
+                            break;
+                        case "z":
+                        case "Z":
+                            chip.key[0xA] = 0;
+                            break;
+                        case "x":
+                        case "X":
+                            chip.key[0x0] = 0;
+                            break;
+                        case "c":
+                        case "C":
+                            chip.key[0xB] = 0;
+                            break;
+                        case "v":
+                        case "V":
+                            chip.key[0xF] = 0;
+                            break;
+                    }
+
+                    chip.isInputExecuted = true;
+                };
+
+                gameWindow.RenderFrame += (sender, e) =>
+                {
+                    gameWindow.Title = (frame % 60).ToString();
+                    frame++;
+
+                    chip.EmulateCycle();
+
+                    GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+                    GL.MatrixMode(MatrixMode.Projection);
+                    GL.LoadIdentity();
+                    GL.Ortho(0, 640, 320, 0, -1, 1);
+
+                    for (int i = 0; i < 64; i++)
+                    {
+                        for (int j = 0; j < 32; j++)
+                        {
+                            if (chip.gfx[i, j])
+                            {
+                                GL.Begin(BeginMode.Quads);
+                                GL.Vertex2(i * 10, j * 10);
+                                GL.Vertex2(i * 10, j * 10 + 10);
+                                GL.Vertex2((i + 1) * 10, j * 10 + 10);
+                                GL.Vertex2((i + 1) * 10, j * 10);
+                                GL.End();
+                            }
+                        }
+                    }
+
+                    gameWindow.SwapBuffers();
+                };
+
+                gameWindow.Run(120.0);
+            }
+            Console.ReadLine();
         }
     }
 }
